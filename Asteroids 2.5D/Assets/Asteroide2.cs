eefd0f5fe1ab0d558a44dbf12ab3e7f5db0cf73c
Zucaritas0-0 <gearman2003@gmail.com>
2023-05-13 21:26:30 -0600

    Cuerpos celestes

---
+++ Asteroids 2.5D/Assets/Asteroide2.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Asteroide2 : MonoBehaviour{
+
+[SerializeField] public float rotationOffset;
+Transform myT;
+Vector3 randomRotation;
+
+void Awake()
+{
+    myT = transform;
+}
+
+void Start()
+
+{
+    randomRotation.x = Random.Range(-rotationOffset, rotationOffset);
+    randomRotation.y = Random.Range(-rotationOffset, rotationOffset);
+    randomRotation.z = Random.Range(-rotationOffset, rotationOffset);
+}
+
+void FixedUpdate()
+{
+    myT.Rotate(randomRotation*Time.deltaTime);
+
+}
+
+}
