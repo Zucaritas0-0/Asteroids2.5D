ff2dd2f3260245bfe294f8de362d9ccef4809318
Zucaritas0-0 <gearman2003@gmail.com>
2023-05-16 19:35:43 -0600

    Intento de creación de asteroides

---
+++ Asteroids 2.5D/Assets/prefabs/Asteroids/Asteroide3.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Asteroide3 : MonoBehaviour
+{
+   public GameObject rock;
+    /*public Gameplay gameplay;*/
+    private float maxRotation;
+    private float rotationX;
+    private float rotationY;
+    private float rotationZ;
+    private Rigidbody rb;
+    private Camera mainCam;
+    private float maxSpeed;
+    private int _generation;
+
+    void Start()
+    {
+
+        mainCam = Camera.main;
+
+        maxRotation = 25f;
+        rotationX = Random.Range(-maxRotation, maxRotation);
+        rotationY = Random.Range(-maxRotation, maxRotation);
+        rotationZ = Random.Range(-maxRotation, maxRotation);
+
+        /*rb = rock.GetComponent();*/
+
+        float speedX = Random.Range(200f, 800f);
+        int selectorX = Random.Range(0, 2);
+        float dirX = 0;
+        if (selectorX == 1) { dirX = -1; }
+        else { dirX = 1; }
+        float finalSpeedX = speedX * dirX;
+        rb.AddForce(transform.right * finalSpeedX);
+
+        float speedY = Random.Range(200f, 800f);
+        int selectorY = Random.Range(0, 2);
+        float dirY = 0;
+        if (selectorY == 1) { dirY = -1; }
+        else { dirY = 1; }
+        float finalSpeedY = speedY * dirY;
+        rb.AddForce(transform.up * finalSpeedY);
+
+    }
+
+    public void SetGeneration(int generation)
+    {
+        _generation = generation;
+    }
+
+    void Update()
+    {
+        rock.transform.Rotate(new Vector3(rotationX, rotationY, 0) * Time.deltaTime);
+        /*CheckPosition();*/
+        float dynamicMaxSpeed = 3f;
+        rb.velocity = new Vector2(Mathf.Clamp(rb.velocity.x, -dynamicMaxSpeed, dynamicMaxSpeed), Mathf.Clamp(rb.velocity.y, -dynamicMaxSpeed, dynamicMaxSpeed));
+    }
+    
+}
