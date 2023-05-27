fdb3b380db812132b69d818b65914bbdffa855d8
Zucaritas0-0 <gearman2003@gmail.com>
2023-05-27 11:34:03 -0600

    Destrucción asteroides

--- Asteroids 2.5D/Assets/prefabs/Asteroids/AsteroideMovement.cs
+++ Asteroids 2.5D/Assets/prefabs/Asteroids/AsteroideMovement.cs
@@ -6,6 +6,7 @@ public class AsteroideMovement : MonoBehaviour
 {
     public Rigidbody rb;
     Vector3 destiny;
+    private Camera cam;
 
 
     // Start is called before the first frame update
@@ -14,6 +15,21 @@ public class AsteroideMovement : MonoBehaviour
         destiny = new Vector3(Random.Range(-10,10),transform.position.y,Random.Range(-10,10))-transform.position;
         rb = GetComponent<Rigidbody>();
         rb.AddForce(destiny.normalized*Random.Range(200,300));
+        cam = Camera.main;
+
         
     }
+
+    private void FixedUpdate()
+    {
+        Vector3 distancia;
+        distancia = transform.position-destiny;
+        if(distancia.magnitude>17){
+            Destroy(this.gameObject);
+        }
+        Debug.Log("distancia:  "+ distancia.magnitude);
+
+    }
+
+
 }
--- Asteroids 2.5D/Assets/prefabs/Asteroids/Generation.cs
+++ Asteroids 2.5D/Assets/prefabs/Asteroids/Generation.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Generation : MonoBehaviour{
 
     public GameObject objeto;
-    public float respawnTime = 1.0f;
+    public float respawnTime = 80.0f;
     public float spawnrange;
 
 
